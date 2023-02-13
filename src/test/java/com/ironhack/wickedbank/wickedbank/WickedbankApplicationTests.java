bb4d7ad3611cad9fa650adfcb11f2a6eb610bcf2
Adrià <adria.lop.mar@gmail.com>
2023-02-13 19:55:05 +0100

    created project

---
+++ src/test/java/com/ironhack/wickedbank/wickedbank/WickedbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.wickedbank.wickedbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WickedbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/ironhack/wickedbank/wickedbank/model/UserTest.java
@@ -0,0 +1,76 @@
+package com.ironhack.wickedbank.wickedbank.model;
+
+import com.ironhack.wickedbank.wickedbank.classes.Address;
+import com.ironhack.wickedbank.wickedbank.classes.Money;
+import com.ironhack.wickedbank.wickedbank.enums.Status;
+import com.ironhack.wickedbank.wickedbank.model.accountType.CreditCard;
+import com.ironhack.wickedbank.wickedbank.model.accountType.Savings;
+import com.ironhack.wickedbank.wickedbank.model.accountType.StudentChecking;
+import com.ironhack.wickedbank.wickedbank.model.userInfo.AccountHolder;
+import com.ironhack.wickedbank.wickedbank.model.userInfo.Admin;
+import com.ironhack.wickedbank.wickedbank.model.userInfo.ThirdParty;
+import com.ironhack.wickedbank.wickedbank.repository.AccountRepository;
+import com.ironhack.wickedbank.wickedbank.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+    private User accountHolder;
+    private User admin;
+    private User thirdParty;
+    private Account studentChecking;
+    private Account creditCard;
+    private Account savings;
+    @BeforeEach
+    void setUp() {
+        Address address =new Address("fake");
+        LocalDate localDate = LocalDate.of(1991, 06, 26);
+        LocalDate creationDate = LocalDate.of(2023, 02, 13);
+        // users ===================================
+        accountHolder= new AccountHolder("adri",localDate,address,"asd@asd.com");
+        userRepository.save(accountHolder);
+        admin = new Admin("Denna");
+        userRepository.save(admin);
+        thirdParty = new ThirdParty("rubik","123asd");
+        userRepository.save(thirdParty);
+
+        // accounts =============================
+        studentChecking = new StudentChecking(new Money(new BigDecimal("10.50")),"123456",accountHolder.getUserId(),creationDate, Status.ACTIVE);
+        accountRepository.save(studentChecking);
+        savings = new Savings(new Money(new BigDecimal("100")),"123456",accountHolder.getUserId(),creationDate,Status.ACTIVE,new BigDecimal("1"));
+        accountRepository.save(savings);
+        creditCard = new CreditCard(new Money(new BigDecimal("50")),"123",accountHolder.getUserId(),creationDate,Status.FROZEN,new Money( new BigDecimal("250")),new BigDecimal("0.285"));
+        accountRepository.save(creditCard);
+
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+//        userRepository.deleteAll();
+//        accountRepository.deleteAll();
+    }
+    @Test
+    public void addUser_AddUserSuccessfully(){
+        assertEquals(3,userRepository.findAll().size());
+    }
+    @Test
+    public void addAccount_AddAccountSuccessfully(){
+        assertEquals(3,accountRepository.findAll().size());
+    }
+}
