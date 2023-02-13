23b597d6f8279cc9b7ddd3da28f287f5c8a1b1e7
Adrià <adria.lop.mar@gmail.com>
2023-02-13 19:53:30 +0100

    created project

---
+++ src/main/java/com/ironhack/wickedbank/wickedbank/WickedbankApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.wickedbank.wickedbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WickedbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WickedbankApplication.class, args);
+	}
+
+}
