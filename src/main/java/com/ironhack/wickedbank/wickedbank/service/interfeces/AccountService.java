f610e5efd7d670f4672904f697da653fa305483d
Adrià <adria.lop.mar@gmail.com>
2023-02-15 16:06:57 +0100

    added account service

---
+++ src/main/java/com/ironhack/wickedbank/wickedbank/service/interfeces/AccountService.java
@@ -0,0 +1,7 @@
+package com.ironhack.wickedbank.wickedbank.service.interfeces;
+
+import com.ironhack.wickedbank.wickedbank.model.Account;
+
+public interface AccountService {
+    Account getAccountById(Long id);
+}
