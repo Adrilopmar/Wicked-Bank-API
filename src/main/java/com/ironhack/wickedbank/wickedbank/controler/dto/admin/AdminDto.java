fe1c4aa8ad6a93b15a87195707d7e641d85c21de
Adrià <adria.lop.mar@gmail.com>
2023-02-16 09:34:19 +0100

    created credit card DTO

---
+++ src/main/java/com/ironhack/wickedbank/wickedbank/controler/dto/admin/AdminDto.java
@@ -0,0 +1,11 @@
+package com.ironhack.wickedbank.wickedbank.controler.dto.admin;
+
+import com.ironhack.wickedbank.wickedbank.classes.User;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+public class AdminDto extends User {
+
+
+
+}
