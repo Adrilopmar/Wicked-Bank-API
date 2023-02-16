18ae095befde9c8c2242d63bed4c5a588727e349
Adrià <adria.lop.mar@gmail.com>
2023-02-16 09:13:27 +0100

    created methods impl (createCreditCard, createAdmin,CreateAccountHolder,CreateThirdParty)

--- src/main/java/com/ironhack/wickedbank/wickedbank/service/impl/AdminServiceImpl.java
+++ src/main/java/com/ironhack/wickedbank/wickedbank/service/impl/AdminServiceImpl.java
@@ -1,17 +1,26 @@
 package com.ironhack.wickedbank.wickedbank.service.impl;
 
 import com.ironhack.wickedbank.wickedbank.classes.Money;
+import com.ironhack.wickedbank.wickedbank.controler.dto.accountholder.create.AccountHolderDto;
+import com.ironhack.wickedbank.wickedbank.controler.dto.admin.AdminDto;
 import com.ironhack.wickedbank.wickedbank.controler.dto.checking.create.CheckingDto;
+import com.ironhack.wickedbank.wickedbank.controler.dto.creditcard.create.CreditCardDto;
 import com.ironhack.wickedbank.wickedbank.controler.dto.savings.create.SavingsDto;
+import com.ironhack.wickedbank.wickedbank.controler.dto.thirdparty.create.ThirdPartyDto;
+import com.ironhack.wickedbank.wickedbank.enums.Role;
 import com.ironhack.wickedbank.wickedbank.enums.Type;
 import com.ironhack.wickedbank.wickedbank.model.Account;
 import com.ironhack.wickedbank.wickedbank.model.User;
 import com.ironhack.wickedbank.wickedbank.model.accountType.Checking;
+import com.ironhack.wickedbank.wickedbank.model.accountType.CreditCard;
 import com.ironhack.wickedbank.wickedbank.model.accountType.Savings;
 import com.ironhack.wickedbank.wickedbank.model.accountType.StudentChecking;
 import com.ironhack.wickedbank.wickedbank.model.userInfo.AccountHolder;
+import com.ironhack.wickedbank.wickedbank.model.userInfo.Admin;
+import com.ironhack.wickedbank.wickedbank.model.userInfo.ThirdParty;
 import com.ironhack.wickedbank.wickedbank.repository.AccountHolderRepository;
 import com.ironhack.wickedbank.wickedbank.repository.AccountRepository;
+import com.ironhack.wickedbank.wickedbank.repository.AdminRepository;
 import com.ironhack.wickedbank.wickedbank.repository.UserRepository;
 import com.ironhack.wickedbank.wickedbank.service.interfeces.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +36,8 @@ import java.util.Optional;
 public class AdminServiceImpl implements AdminService {
     @Autowired
     AccountRepository accountRepository;
+    @Autowired
+    AdminRepository adminRepository;
     @Autowired
     AccountHolderRepository accountHolderRepository;
     @Autowired
@@ -78,18 +89,24 @@ public class AdminServiceImpl implements AdminService {
         Checking account = new Checking(); // create new checking
         List<User> owners =new ArrayList(); // create list for owners
         List<Account> accountList = new ArrayList<>();
+
         Optional<AccountHolder> optionalHolder = accountHolderRepository.findById(dto.getOwnerId());
-        if (optionalHolder.isPresent()){ //check if user exist in db
+        if(checkUserInDatabase(dto.getOwnerId())){
             owners.add(optionalHolder.get());
-        }else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found");
         }
+//        if (optionalHolder.isPresent()){ //check if user exist in db
+//        }else {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found");
+//        }
         if (dto.getSecondaryOwner() != null){
             Optional<User> optionalSecondOwner = userRepository.findById(dto.getSecondaryOwner());
-            if (optionalSecondOwner.isPresent()){
+//            if (optionalSecondOwner.isPresent()){
+//                owners.add(optionalSecondOwner.get());
+//            }else {
+//                throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Secondary owner not found");
+//            }
+            if(checkUserInDatabase(optionalSecondOwner.get().getUserId())){
                 owners.add(optionalSecondOwner.get());
-            }else {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Secondary owner not found");
             }
         }
         if(account.getCreationDate().getYear()-optionalHolder.get().getDateOfBirth().getYear() <24){
@@ -122,6 +139,83 @@ public class AdminServiceImpl implements AdminService {
             account.setOwner(dto.getOwnerId());
             account.setSecretKey(dto.getSecretKey());
             accountRepository.save(account);
+        }
+    }
+    public CreditCard createCreditCard(CreditCardDto dto) {
+        CreditCard account = new CreditCard(); // create new credit card
+        List<User> owners =new ArrayList(); // create list for owners
+        List<Account> accountList = new ArrayList<>();
+        Optional<AccountHolder> optionalHolder = accountHolderRepository.findById(dto.getOwnerId());
+        if(checkUserInDatabase(dto.getOwnerId())){
+            owners.add(optionalHolder.get());
+        }
+        if (dto.getSecondaryOwner() != null){
+            Optional<User> optionalSecondOwner = userRepository.findById(dto.getSecondaryOwner());
+            if(checkUserInDatabase(optionalSecondOwner.get().getUserId())){
+                owners.add(optionalSecondOwner.get());
+            }
+        }
+        accountList.add(account);
+        optionalHolder.get().setAccounts(accountList);
+        if(dto.getBalance() != null){ //setting balance & checking minimum
+            if (dto.getBalance().getAmount().compareTo(new BigDecimal("100"))<0) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Balance can not be less than 100");
+            } else if (dto.getBalance().getAmount().compareTo(new BigDecimal("100000"))>0) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Balance can not be higher than 100000");
+            } else {
+                account.setBalance(dto.getBalance());
+            }
+        }
+        account.setType(Type.CREDIT_CARD);
+        account.setOwners(owners);
+        account.setOwner(dto.getOwnerId());
+        account.setSecretKey(dto.getSecretKey());
+        accountRepository.save(account);
+        return account;
+    }
+    public Admin createAdmin(AdminDto adminDto) {
+        Admin admin = new Admin();
+        Optional<Admin> optionalAdmin = Optional.ofNullable(adminRepository.findAllByRole(Role.ADMIN));
+        if (!optionalAdmin.isPresent()){
+            admin.setName(adminDto.getName());
+            admin.setPassword(adminDto.getPassword());
+            userRepository.save(admin);
+            return admin;
+        }else {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN,"This application only supports 1 admin.");
+        }
+    }
+    public AccountHolder createAccountHolder(AccountHolderDto dto) {
+        AccountHolder accountHolder = new AccountHolder();
+        accountHolder.setName(dto.getName());
+        accountHolder.setPassword(dto.getPassword());
+        if (dto.getAddress()!= null){
+            accountHolder.setAddress(dto.getAddress());
+        }
+        if (dto.getDateOfBirth()!=null){
+            accountHolder.setDateOfBirth(dto.getDateOfBirth());
+        }
+        if (dto.getMailingAddress()!=null){
+            accountHolder.setMailingAddress(dto.getMailingAddress());
+        }
+        userRepository.save(accountHolder);
+        return accountHolder;
+    }
+
+    public ThirdParty createThirdParty(ThirdPartyDto dto) {
+       ThirdParty thirdParty = new ThirdParty();
+       thirdParty.setHashedKey(dto.getHashedKey());
+       thirdParty.setPassword(dto.getPassword());
+       thirdParty.setName(dto.getName());
+       return thirdParty;
+    }
+
+    public Boolean checkUserInDatabase(Long userId){
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()){ //check if user exist in db
+            return true;
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found");
         }
     }
 }
